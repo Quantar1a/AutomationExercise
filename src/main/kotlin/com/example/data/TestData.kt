4c4af15c5665ff171b6774fd6495ea87ad804b6a
Quantaria <quantar1a@outlook.com>
2024-12-26 19:13:17 +0200

    Added Faker framework for generating random data

--- src/main/kotlin/com/example/data/TestData.kt
+++ src/main/kotlin/com/example/data/TestData.kt
@@ -1,10 +1,19 @@
 package com.example.data
 
+import com.github.javafaker.Faker
+
 class TestData
 {
     companion object
     {
         //Main URL of the Automation Exercise
         val url: String = "https://www.automationexercise.com"
+
+        var emal : String = TestData().generateEmail()
+    }
+
+    fun generateEmail() : String
+    {
+        return Faker().internet().emailAddress()
     }
 }
